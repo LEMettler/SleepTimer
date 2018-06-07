80689db39f807d22a9f0e6d23cfd3a87202d8562
Lmeerlu <l.meerlu@gmx.de>
2018-06-07 17:23:44 +0200

    ui + show time in ufc-Standard

--- app/src/main/java/comlmeerlu/github/sleeptimer/MainActivity.java
+++ app/src/main/java/comlmeerlu/github/sleeptimer/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -18,18 +19,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements NumberPicker.OnClickListener{
 
-   private TextView txtCountdown;
    private TextView txtOnOff;
-   private EditText inputHours;
-   private EditText inputMinutes;
-   private EditText inputSeconds;
+
    private Switch sWifi;
    private Switch sAir;
    private Switch sBlue;
    private Button btnStop;
    private Button btnReset;
    private Button btnStart;
-   private Button btnTest;
+   private Button btnInput;
 
 
     private WifiManager wifiManager;
@@ -38,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnCl
 
     private long secUntilFinished;
     private int seconds;
+    private int startSec;
     private boolean counting;
     private boolean stop;
     private boolean finished;
@@ -55,61 +54,15 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnCl
         stop = false;
         finished = true;
 
-        inputHours = findViewById(R.id.inputHours);
-        inputMinutes = findViewById(R.id.inputMinutes);
-        inputSeconds = findViewById(R.id.inputSeconds);
-        seconds = getTime();
+        startSec = 30 * 60;
+        seconds = startSec;
 
         btnStart = findViewById(R.id.btnCountdown);
         btnReset = findViewById(R.id.btnReset);
         btnStop = findViewById(R.id.btnStop);
 
-        //* *****************************************************************************************
-        btnTest = findViewById(R.id.btnTest);
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Dialog d = new Dialog(MainActivity.this);
-                d.setTitle("Countdown Picker");
-                d.setContentView(R.layout.dialog);
-
-                Button btnDelete = (Button) d.findViewById(R.id.btnDelete);
-                Button btnSave = (Button) d.findViewById(R.id.btnSave);
-
-                final NumberPicker pickMin = (NumberPicker) d.findViewById(R.id.pickMin);
-                pickMin.setMaxValue(100);
-                pickMin.setMinValue(1);
-                pickMin.setWrapSelectorWheel(false);
-
-                final NumberPicker pickSec = (NumberPicker) d.findViewById(R.id.pickSec);
-                pickSec.setMaxValue(59);
-                pickSec.setMinValue(0);
-                pickSec.setWrapSelectorWheel(false);
-
-                btnSave.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        seconds = pickSec.getValue();
-                        seconds += pickMin.getValue()*60;
-                        d.dismiss();
-                    }
-                });
-                btnDelete.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        d.dismiss();
-                    }
-                });
-                d.show();
-
-        }
-        });
-        //******************************************************************************************
-
-        txtCountdown = findViewById(R.id.txtCountdown);
-        txtCountdown.setText(String.format("%d",seconds));
-        txtOnOff = findViewById(R.id.txtOnOff);
+        btnInput = findViewById(R.id.btnInput);
+        btnInput.setText(secToUFCStandard(startSec));
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
         sWifi = findViewById(R.id.switchWIFI);
@@ -117,15 +70,77 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnCl
         sAir = findViewById(R.id.switchAir);
     }
 
-    private int getTime(){
-        int r = Integer.parseInt(inputHours.getText().toString()) * 60 * 60;
-        r = r + Integer.parseInt(inputMinutes.getText().toString()) * 60;
-        r = r + Integer.parseInt(inputSeconds.getText().toString());
 
-        return r;
+    public void countdownInputHandler(View view){
+
+        final Dialog d = new Dialog(MainActivity.this);
+        d.setTitle("Countdown Picker");
+        d.setContentView(R.layout.dialog);
+
+        Button btnDelete = (Button) d.findViewById(R.id.btnDelete);
+        Button btnSave = (Button) d.findViewById(R.id.btnSave);
+
+        final NumberPicker pickHours = (NumberPicker) d.findViewById(R.id.pickHours);
+        pickHours.setMaxValue(9);
+        pickHours.setMinValue(0);
+        pickHours.setWrapSelectorWheel(false);
+
+        final NumberPicker pickMin = (NumberPicker) d.findViewById(R.id.pickMin);
+        pickMin.setMaxValue(59);
+        pickMin.setMinValue(0);
+        pickMin.setWrapSelectorWheel(false);
+        pickMin.setValue(30);
+
+        final NumberPicker pickSec = (NumberPicker) d.findViewById(R.id.pickSec);
+        pickSec.setMaxValue(59);
+        pickSec.setMinValue(1);
+        pickSec.setWrapSelectorWheel(false);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSec = pickSec.getValue();
+                startSec += pickMin.getValue()*60;
+                startSec += pickHours.getValue()*60*60;
+                btnInput.setText(secToUFCStandard(startSec));
+
+                d.dismiss();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                d.dismiss();
+            }
+        });
+        d.show();
     }
 
+    private String secToUFCStandard(int totalSeconds){
+        final int MINUTES_IN_AN_HOUR = 60;
+        final int SECONDS_IN_A_MINUTE = 60;
 
+        int seconds = totalSeconds % SECONDS_IN_A_MINUTE;
+        int totalMinutes = totalSeconds / SECONDS_IN_A_MINUTE;
+        int minutes = totalMinutes % MINUTES_IN_AN_HOUR;
+        int hours = totalMinutes / MINUTES_IN_AN_HOUR;
+
+        String ufcTime = "";
+        if (hours < 10) {
+            ufcTime = "0";
+        }
+        ufcTime += Integer.toString(hours) + ":";
+        if (minutes < 10) {
+            ufcTime += "0";
+        }
+        ufcTime += Integer.toString(minutes) + ":";
+        if (seconds < 10) {
+            ufcTime += "0";
+        }
+        ufcTime += Integer.toString(seconds);
+
+        return ufcTime;
+    }
 
     private void initCountdownTimer(int time){
         countDownTimer = new CountDownTimer(1000 * time, 1000) {
@@ -137,7 +152,7 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnCl
                 }else {
                     //Integer.toString can't be applied to textviews, bc it doesn't regard some specific formats
                     secUntilFinished = (millisUntilFinished / 1000);
-                    txtCountdown.setText(String.format("%d", (int) secUntilFinished) + " sec");
+                    btnInput.setText(secToUFCStandard((int) secUntilFinished));
                 }
             }
 
@@ -191,20 +206,24 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnCl
         if (counting) {
             stop = true;
         }
+
         counting = false;
         btnStart.setVisibility(View.VISIBLE);
         btnStop.setVisibility(View.GONE);
         btnStop.setText("STOP");
         btnReset.setVisibility(View.GONE);
+        btnInput.setClickable(false);
+        btnInput.setText(secToUFCStandard(startSec));
     }
 
     public void btnStartHandler(View view) throws InterruptedException {
 
-        initCountdownTimer(getTime());
+        initCountdownTimer(startSec);
         countDownTimer.start();
         btnStart.setVisibility(View.GONE);
         btnStop.setVisibility(View.VISIBLE);
         btnReset.setVisibility(View.VISIBLE);
+        btnInput.setClickable(false);
         counting = true;
     }
 
